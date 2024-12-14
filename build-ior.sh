c1da46add15b360bf81b7b449e6145f0d3463ac9
lucaparisi91 <luca.parisi91@gamil.com>
2024-12-14 12:09:56 +0000

    Added script for building ior

---
+++ build-ior.sh
@@ -0,0 +1,7 @@
+cd ior
+module load PrgEnv-gnu
+module load cray-hdf5-parallel
+
+CC=cc ./configure --prefix=/work/z19/z19/lparisi/courses/io/ior_tests/ior --with-lustre --with-posix --with-hdf5
+make 
+make install
